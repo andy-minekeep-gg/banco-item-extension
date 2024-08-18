8c2dc36f93e8fe01cd5cc57f51bd3b3a969781fa
U8092 <uisl8092@gmail.com>
2024-08-18 16:20:26 +0200

    Initial commit

---
+++ src/main/java/ovh/mythmc/banco_extension_example/BancoExtensionExample.java
@@ -0,0 +1,17 @@
+package ovh.mythmc.banco_extension_example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BancoExtensionExample extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
